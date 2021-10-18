9eee67852c6a783b9b2250e1c855b5638c528bb7
Zach Eriksen <zmeriksen@icloud.com>
2021-10-18 17:31:46 -0600

    Initial Commit

---
+++ Onion-POC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Onion-POC
+//
+//  Created by Leif on 10/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Onion-POC/Onion_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onion_POCApp.swift
+//  Onion-POC
+//
+//  Created by Leif on 10/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Onion_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
